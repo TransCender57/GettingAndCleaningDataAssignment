b7bed23e1595fca60dcc8ce9a1636dc9c2aec4f5
David White <transcender57@gmail.com>
2016-04-08 21:16:13 -0600

    Cleaned up code

--- run_analysis.R
+++ run_analysis.R
@@ -41,27 +41,20 @@ combined <- rbind(training, testing)
 measurementsToKeep <- grep("-(mean|std)\\(\\)", names(combined))
 keep <- combined[, c(1, 2, measurementsToKeep)]
 
-# Now compute the mean for the measurements for each subject regardless of activity
-averagesPerSubject <- keep %>%
-  group_by(Subject) %>%
-  summarise_each(funs(mean(., na.rm = TRUE, -Activity)))
-
-# Now compute the mean for the measurements for each activity regardless of subject
-averagesPerActivity <- keep %>%
-  group_by(Activity) %>%
-  summarise_each(funs(mean(., na.rm = TRUE, -Subject)))
+names(keep) <- gsub("-", "_", names(keep))
+names(keep) <- gsub("^t", "time_", names(keep))
+names(keep) <- gsub("^f", "frequency_", names(keep))
+names(keep) <- gsub("Acc", "Accelerometer", names(keep))
+names(keep) <- gsub("Gyro", "Gyroscope", names(keep))
+names(keep) <- gsub("Mag", "Magnitude", names(keep))
+names(keep) <- gsub("BodyBody", "Body", names(keep))
+names(keep) <- gsub("\\(\\)", "", names(keep))
 
 # Now compute the mean for the measurements for each subject and activity
 averagesPerSubjectAndActivity <- keep %>%
   group_by(Subject, Activity) %>%
   summarise_each(funs(mean(., na.rm = TRUE)))
 
-# For the last summary make the feature names more human readable
-names(averagesPerSubjectAndActivity) <- gsub("-", "_", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("^t", "time_", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("^f", "frequency_", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("Acc", "Accelerometer", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("Gyro", "Gyroscope", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("Mag", "Magnitude", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("BodyBody", "Body", names(averagesPerSubjectAndActivity))
-names(averagesPerSubjectAndActivity) <- gsub("\\(\\)", "", names(averagesPerSubjectAndActivity))
+df <- data.frame(averagesPerSubjectAndActivity)
+
+write.table(df, file = "AveragesPerSubjectAndActivity.txt", row.name = FALSE, sep = "\t")
